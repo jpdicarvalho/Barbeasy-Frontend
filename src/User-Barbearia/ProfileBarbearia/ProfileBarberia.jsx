84dc14351d00dd5941fa6e18459f4ff711d9b59f
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-01-18 19:16:56 -0300

    upgrade method send image auto

--- src/User-Barbearia/ProfileBarbearia/ProfileBarberia.jsx
+++ src/User-Barbearia/ProfileBarbearia/ProfileBarberia.jsx
@@ -194,7 +194,7 @@ function ProfileBarbearia() {
     const timeout = setTimeout(() => {
       // Executa a função de upload após o período de espera
       handleBannerImagesUpload();
-    }, 3000);
+    }, 1000);
 
     // Limpa o temporizador se o componente for desmontado ou se houver uma nova mudança no input de arquivo
     return () => clearTimeout(timeout);
