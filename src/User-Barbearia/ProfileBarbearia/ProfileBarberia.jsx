1a53b7da232e21cf7757e3bbd6cffdc2524ba56e
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-03-08 16:52:06 -0300

    Update ProfileBarberia.jsx

--- src/User-Barbearia/ProfileBarbearia/ProfileBarberia.jsx
+++ src/User-Barbearia/ProfileBarbearia/ProfileBarberia.jsx
@@ -1201,7 +1201,7 @@ const formatarPreco = (valor) => {
                     onChange={handleFile}
                   />
 
-                  {imageUser[0] != "https://d15o6h0uxpz56g.cloudfront.net/user_image" ? (
+                  {imageUser.length > 48 ? (
                     <div className="img-view-profile">
                       <img src={imageUser} alt="" id='img-profile' />
                     </div>
