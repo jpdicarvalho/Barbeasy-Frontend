50ab6678754f2d25a9a0b560dd7d9ac11a241086
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-03-07 22:08:30 -0300

    Update Home.jsx

--- src/User-Client-Barbearia/Home/Home.jsx
+++ src/User-Client-Barbearia/Home/Home.jsx
@@ -124,7 +124,7 @@ const avaliacoesDaBarbearia = AllAvaliation.filter(avaliacao => avaliacao.barbea
   const media = somaNotas / avaliacoesDaBarbearia.length;
   return media.toFixed(1).replace('.', ',');
 };
-
+console.log(barbeariaSearch)
 return (
           <div className="containerHome">
 
