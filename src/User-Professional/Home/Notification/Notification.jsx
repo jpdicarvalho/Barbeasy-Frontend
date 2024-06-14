699162d9d328c3f0c2408b5bdb237cff5edf79f7
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-06-14 17:43:45 -0300

    Update Notification.jsx

--- src/User-Professional/Home/Notification/Notification.jsx
+++ src/User-Professional/Home/Notification/Notification.jsx
@@ -4,10 +4,28 @@ import './Notification.css'
 
 export default function Notification ({openNotification, setCloseNotification}){
 
+    const token = localStorage.getItem('token');
+    const userData = localStorage.getItem('dataprofessional');//Obtendo os dados salvo no localStorage
+    const userInformation = JSON.parse(userData);//trasnformando os dados para JSON
+    const professionalId = userInformation.professional[0].id;
+
     const urlApi = 'https://barbeasy.up.railway.app'
     
-    const
+    const[Notification, setShowNotification] = useState([])
 
+    useEffect(() =>{
+        axios.get(`${urlApi}/api/v1/allNotification/${professionalId}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+              }
+        }).then(res =>{
+            if(res.data.Success === 'Success'){
+                setShowNotification(res.data.AllNotification)
+            }
+        }).catch(err =>{
+            console.log("Error", err)
+        })
+    })
     if(openNotification){
         return (
             <>
