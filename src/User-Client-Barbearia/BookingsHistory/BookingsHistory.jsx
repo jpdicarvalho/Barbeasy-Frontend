404df1057f44f1b30272fe3eee57449016471649
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-07-06 20:05:47 -0300

    update function to get bookings of user

--- src/User-Client-Barbearia/BookingsHistory/BookingsHistory.jsx
+++ src/User-Client-Barbearia/BookingsHistory/BookingsHistory.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom"
+import axios from 'axios';
 
 import './BookingsHistory.css'
 
@@ -8,11 +9,45 @@ import { BsFillGridFill } from "react-icons/bs";
 import { IoIosSearch } from "react-icons/io";
 
 function BookingsHistory (){
+    const urlApi = 'https://barbeasy.up.railway.app'
+
     const navigate = useNavigate();
+
+    //Buscando informações do usuário logado
+    const token = localStorage.getItem('token');
+    const userDataFromLocalStorage = localStorage.getItem('userData');//Obtendo os dados salvo no localStorage
+    const userInformation = JSON.parse(userDataFromLocalStorage);//trasnformando os dados para JSON
+    const userId = userInformation.user[0].id;
 
     const handleBackClick = () => {
         navigate("/Home");
       };
+
+    const [allBookings, setAllBookings] = useState ([]);
+    const [message, setMessage] = useState ('');
+
+    const getAllBookings = () =>{
+        axios.get(`${urlApi}/api/v1/bookingsOfUser/:userId`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+              }
+        }).then(res =>{
+            if(res.data.Success === 'Success'){
+                setAllBookings(res.data.Bookings)
+            }else{
+                setMessage('Nenhum agendamento encontrado.')
+                setTimeout(() => {
+                    setMessage('');
+                }, 3000);
+            }
+        }).catch(err =>{
+            console.error('Erro ao buscar agendamentos', err)
+        })
+    }
+    
+    useEffect(() =>{
+        getAllBookings()
+    }, [])
 
     return(
         <>
@@ -27,7 +62,6 @@ function BookingsHistory (){
                     <div className='Box__input__Search'>
                         <IoIosSearch id='lupa__in__bookings__history'/>
                         <input type="search" className='Inner__input__search' placeholder='Buscar agendamento'/>
-                        <BsFillGridFill className="icon__filter"/>
                     </div>
                 </div>
             </div>
--- src/User-Client-Barbearia/UserProfile/UserProfile.jsx
+++ src/User-Client-Barbearia/UserProfile/UserProfile.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
-import {motion} from 'framer-motion';
 
 import axios from 'axios';
 
