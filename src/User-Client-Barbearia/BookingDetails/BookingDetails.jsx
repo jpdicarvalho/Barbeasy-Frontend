64eabe90c5abd7746bdc67187e752796c6fccdef
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-07-08 13:12:09 -0300

    update style bookings details

--- src/User-Client-Barbearia/BookingDetails/BookingDetails.jsx
+++ src/User-Client-Barbearia/BookingDetails/BookingDetails.jsx
@@ -3,18 +3,26 @@ import { useState, useEffect } from "react";
 
 import { BsCalendar2 } from "react-icons/bs";
 import { TfiTime } from "react-icons/tfi";
+import { GiRazor } from "react-icons/gi";
+import { VscCalendar } from "react-icons/vsc";
+import { MdOutlinePhone } from "react-icons/md";
 
 import './BookingDetails.css'
 
 
 function BookingDetails () {
+
     const navigate = useNavigate();
     const location = useLocation();
 
     const { booking } = location.state;
-    console.log(booking)
     
     const urlCloudFront = 'https://d15o6h0uxpz56g.cloudfront.net/'
+
+    const handleBackClick = () => {
+        navigate("/BookingsHistory");
+    };
+
     return(
         <>
             <div className="container__Booking__Details">
@@ -39,24 +47,39 @@ function BookingDetails () {
                         <TfiTime className="icon__TfiTime"/>
                         <p>{booking.bookingTime.split(',')[0]} - {booking.bookingTime.split(',')[booking.bookingTime.split(',').length-1]}</p>
                     </div>
+                    
                 </div>
-                <div>
-                    <p>{booking.serviceName}</p>
-                    <p>{booking.servicePrice}</p>
-                    <p>{booking.dateMakedBooking}</p>
-
-
+                <div className="details__service__Booking__Details">
+                    <div className="inner__details__service__Booking__Details">
+                        <GiRazor className="icon__details__service__Booking__Details"/>
+                        <p>{booking.serviceName} • {booking.servicePrice}</p>
+                    </div>
+                    <div className="inner__details__service__Booking__Details">
+                        <VscCalendar className="icon__details__service__Booking__Details"/>
+                        <p style={{paddingTop:'2px'}}> Date e hora de realização do agendamento: {booking.dateMakedBooking}</p>
+                    </div>
+                    
                 </div>
                 
                 <div className="subtittle__Booking__Details">
-                        <h3>Detalhes do profissional</h3>
+                        <h3>Profissional do serviço</h3>
                     </div>
                 <div className="section__professional__Booking__Details">
                     <div className="box__professional__Booking__Details">
                         <img className="img__professional__Booking__Details" src={urlCloudFront + booking.userImageProfessional} alt="" />
                     </div>
-                    <p className="name__professional__Booking__Details">{booking.professionalName}</p>
-                    <p className="phone__Booking__Details">{booking.professionalPhone}</p>
+                    <div className="inner__professional__Booking__Details">
+                        <p className="name__professional__Booking__Details">{booking.professionalName}</p>
+                        <p className="phone__Booking__Details">
+                            <MdOutlinePhone className="icon__details__service__Booking__Details"/>
+                            {booking.professionalPhone}</p>
+                    </div>
+                   
+                </div>
+                <div className="Box__btn__back__Booking__Details">
+                    <button className="Btn__back__Booking__Details" onClick={handleBackClick}>
+                        Voltar
+                    </button>
                 </div>
                 
             </div>
