d70cbe3324163e93909290a2a9a03466045951a7
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-07-19 23:43:44 -0300

    create requst unlink barbearia

--- src/User-Professional/Home/HomeProfessional.jsx
+++ src/User-Professional/Home/HomeProfessional.jsx
@@ -11,7 +11,9 @@ import { IoIosNotifications } from "react-icons/io";
 import { GrSchedules } from "react-icons/gr";
 import { SlOptionsVertical } from "react-icons/sl";
 import { IoIosRemoveCircleOutline } from "react-icons/io";
-
+import { PiPassword } from "react-icons/pi";
+import { MdOutlineDone } from "react-icons/md";
+import { VscError } from "react-icons/vsc";
 
 const monthNames = [
   'Jan', 'Fev', 'Mar', 'Abr', 'Maio', 'Jun', 'Jul', 'Aug', 'Set', 'Out', 'Nov', 'Dez'
@@ -42,207 +44,243 @@ function HomeProfessional() {
   const professionalUserName = userInformation.professional[0].name;
   const firstLetter = professionalUserName.charAt(0).toUpperCase();
 
-const navigateToProfileProfessional = () =>{
-  navigate("/ProfileProfessional");
-}
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-const navigateToNotification = () =>{
-  navigate("/Notification");
-}
+
+  const navigateToProfileProfessional = () =>{
+    navigate("/ProfileProfessional");
+  }
+
+  const navigateToNotification = () =>{
+    navigate("/Notification");
+  }
 
 //==================== GET NOTIFICATION ================
-const[notification, setNotification] = useState([]);
+  const[notification, setNotification] = useState([]);
 
-    //function to get all notification
-    const getAllnotification = () =>{
-        axios.get(`${urlApi}/api/v1/notificationToProfe/${professionalId}`, {
-            headers: {
-                'Authorization': `Bearer ${token}`
-              }
-        }).then(res =>{
-            if(res.data.Success === 'true'){
-              setNotification(res.data.AllNotification)
+  //function to get all notification
+  const getAllnotification = () =>{
+      axios.get(`${urlApi}/api/v1/notificationToProfe/${professionalId}`, {
+          headers: {
+              'Authorization': `Bearer ${token}`
             }
-        }).catch(err =>{
-            console.log("Error", err)
-        })
-    }
+      }).then(res =>{
+          if(res.data.Success === 'true'){
+            setNotification(res.data.AllNotification)
+          }
+      }).catch(err =>{
+          console.log("Error", err)
+      })
+  }
 
-    useEffect(() =>{
-        getAllnotification()
-    }, [])
+  useEffect(() =>{
+      getAllnotification()
+  }, [])
 
 //============== GET IMAGE PROFESSIONAL ============
-const [imageUser, setImageUser] = useState([]);
+  const [imageUser, setImageUser] = useState([]);
 //Função para obter as imagens cadastradas
-useEffect(() => {
-  axios.get(`${urlApi}/api/v1/userImageProfessional`, {
-    params: {
-      professionalId: professionalId
-    },
-    headers: {
-      'Authorization': `Bearer ${token}`
-    }
-  })
-  .then(res => {
-    setImageUser(res.data.url);
-  })
-  .catch(err => console.log(err));
-}, [professionalId]);
-
-//==================================================
-const [saudacao, setSaudacao] = useState('');
-//pegando a hora para saudar o usuário
-useEffect(() => {
-  const obterSaudacao = () => {
-  const horaAtual = new Date().getHours();
-    if (horaAtual >= 5 && horaAtual < 12) {
-        setSaudacao('Bom dia!');
-    } else if (horaAtual >= 12 && horaAtual < 18) {
-        setSaudacao('Boa tarde!');
-    } else {
-        setSaudacao('Boa noite!');
-    }
-  }
-obterSaudacao();
-}, []);
-
-//==================================================
-//Função para pegar os dias da semana
-const [selectedDay, setSelectedDay] = useState(null);
-const [bookings, setBookings] = useState([]);
-const [expandedCardBooking, setExpandedCardBooking] = useState([]);
-const [messagemNotFound, setMessagemNotFound] = useState("");
-
-
-function getWeeks() {
-  const arrayWeeks = [];
-  const startIndex = weekNames.indexOf(dayOfWeek);
-  const lastDayToShow = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 30);
-
-  for (let i = 0; i < 30; i++) {
-    const currentDay = new Date(date.getFullYear(), date.getMonth(), date.getDate() + i);
-    const index = (startIndex + i) % 7;
-    const nameWeek = weekNames[index];
-
-    if (currentDay <= lastDayToShow) {
-      arrayWeeks.push(nameWeek);
-    }
-  }
-
-  return arrayWeeks;
-}
-
-//Função para gerar a quantidade de dias que a agenda vai ficar aberta
-function getNumber() {
-  const numbersWeek = [];
-  const lastDayToShow = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 30);
-
-  for (let i = 0; i < 30; i++) {
-    const currentDay = new Date(date.getFullYear(), date.getMonth(), date.getDate() + i);
-    const numberWeek = currentDay.getDate();
-    const isCurrentDay = currentDay.toDateString() === date.toDateString();
-    const month = monthNames[currentDay.getMonth()]; // Obtém o nome do mês
-
-    if (currentDay <= lastDayToShow) {
-      numbersWeek.push({
-        number: numberWeek,
-        isCurrentDay: isCurrentDay,
-        month: month, // Adiciona o nome do mês ao objeto
-      });
-    }
-  }
-
-  return numbersWeek;
-}
-
-function orderBookings(bookings) {
-  // Ordenação dos bookings por menor horário (booking_time)
-  bookings.sort((a, b) => {
-    const [horaA, minutoA] = a.booking_time.split(',')[0].split(':'); // Primeiro horário de a
-    const [horaB, minutoB] = b.booking_time.split(',')[0].split(':'); // Primeiro horário de b
-
-    const horarioCompletoA = Number(horaA) * 60 + Number(minutoA); // Horário completo em minutos para a
-    const horarioCompletoB = Number(horaB) * 60 + Number(minutoB); // Horário completo em minutos para b
-
-    // Comparação de horários completos (menor para maior)
-    if (horarioCompletoA < horarioCompletoB) {
-      return -1; // a vem antes de b
-    } else if (horarioCompletoA > horarioCompletoB) {
-      return 1; // b vem antes de a
-    } else {
-      return 0; // horários são iguais
-    }
-  });
-}
-
-const weekDays = getWeeks();
-const numberDays = getNumber();
-//const currentDay = getCurrentDayOfWeek()
-
-const handleDateClick = (dayOfWeek, day, month, year, barbeariaId) => {
-  setSelectedDay(`${dayOfWeek}, ${day} de ${month} de ${year}`);
-  let selectedDate = `${dayOfWeek}, ${day} de ${month} de ${year}`;
-
-    axios.get(`${urlApi}/api/v1/professionalBookings/${barbeariaId}/${professionalId}/${selectedDate}`, {
+  useEffect(() => {
+    axios.get(`${urlApi}/api/v1/userImageProfessional`, {
+      params: {
+        professionalId: professionalId
+      },
       headers: {
         'Authorization': `Bearer ${token}`
       }
     })
     .then(res => {
-      if(res.data.Message === "true"){
-        setBookings(res.data.bookings);
-        // Chamando a função para ordenar os bookings por menor horário
-        orderBookings(bookings);
-      }else{
-        setBookings([])
-        setMessagemNotFound("Nenhum agendamento encontrado")
-      }
+      setImageUser(res.data.url);
     })
     .catch(err => console.log(err));
-}
+  }, [professionalId]);
 
-//Function to expanded booking cards
-const toggleItem = (itemId) => {
-    if (expandedCardBooking.includes(itemId)) {
-      setExpandedCardBooking(expandedCardBooking.filter(id => id !== itemId));
-    } else {
-      setExpandedCardBooking([...expandedCardBooking, itemId]);
+//==================================================
+  const [saudacao, setSaudacao] = useState('');
+//pegando a hora para saudar o usuário
+  useEffect(() => {
+    const obterSaudacao = () => {
+    const horaAtual = new Date().getHours();
+      if (horaAtual >= 5 && horaAtual < 12) {
+          setSaudacao('Bom dia!');
+      } else if (horaAtual >= 12 && horaAtual < 18) {
+          setSaudacao('Boa tarde!');
+      } else {
+          setSaudacao('Boa noite!');
+      }
     }
-};
-//=========== GET BARB TO PROFESSIONAL ==============
-const [barbearias, setBarbearias] = useState([]);
-const [barbeariaSelected, setBarbeariaSelected] = useState();
+  obterSaudacao();
+  }, []);
 
-const getBarbearias = () =>{
-  axios.get(`${urlApi}/api/v1/listBarbeariaToProfessional/${professionalId}`, {
+//==================================================
+//Função para pegar os dias da semana
+  const [selectedDay, setSelectedDay] = useState(null);
+  const [bookings, setBookings] = useState([]);
+  const [expandedCardBooking, setExpandedCardBooking] = useState([]);
+  const [messagemNotFound, setMessagemNotFound] = useState("");
+
+
+  function getWeeks() {
+    const arrayWeeks = [];
+    const startIndex = weekNames.indexOf(dayOfWeek);
+    const lastDayToShow = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 30);
+
+    for (let i = 0; i < 30; i++) {
+      const currentDay = new Date(date.getFullYear(), date.getMonth(), date.getDate() + i);
+      const index = (startIndex + i) % 7;
+      const nameWeek = weekNames[index];
+
+      if (currentDay <= lastDayToShow) {
+        arrayWeeks.push(nameWeek);
+      }
+    }
+
+    return arrayWeeks;
+  }
+
+//Função para gerar a quantidade de dias que a agenda vai ficar aberta
+  function getNumber() {
+    const numbersWeek = [];
+    const lastDayToShow = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 30);
+
+    for (let i = 0; i < 30; i++) {
+      const currentDay = new Date(date.getFullYear(), date.getMonth(), date.getDate() + i);
+      const numberWeek = currentDay.getDate();
+      const isCurrentDay = currentDay.toDateString() === date.toDateString();
+      const month = monthNames[currentDay.getMonth()]; // Obtém o nome do mês
+
+      if (currentDay <= lastDayToShow) {
+        numbersWeek.push({
+          number: numberWeek,
+          isCurrentDay: isCurrentDay,
+          month: month, // Adiciona o nome do mês ao objeto
+        });
+      }
+    }
+
+    return numbersWeek;
+  }
+
+  function orderBookings(bookings) {
+    // Ordenação dos bookings por menor horário (booking_time)
+    bookings.sort((a, b) => {
+      const [horaA, minutoA] = a.booking_time.split(',')[0].split(':'); // Primeiro horário de a
+      const [horaB, minutoB] = b.booking_time.split(',')[0].split(':'); // Primeiro horário de b
+
+      const horarioCompletoA = Number(horaA) * 60 + Number(minutoA); // Horário completo em minutos para a
+      const horarioCompletoB = Number(horaB) * 60 + Number(minutoB); // Horário completo em minutos para b
+
+      // Comparação de horários completos (menor para maior)
+      if (horarioCompletoA < horarioCompletoB) {
+        return -1; // a vem antes de b
+      } else if (horarioCompletoA > horarioCompletoB) {
+        return 1; // b vem antes de a
+      } else {
+        return 0; // horários são iguais
+      }
+    });
+  }
+
+  const weekDays = getWeeks();
+  const numberDays = getNumber();
+//const currentDay = getCurrentDayOfWeek()
+
+  const handleDateClick = (dayOfWeek, day, month, year, barbeariaId) => {
+    setSelectedDay(`${dayOfWeek}, ${day} de ${month} de ${year}`);
+    let selectedDate = `${dayOfWeek}, ${day} de ${month} de ${year}`;
+
+      axios.get(`${urlApi}/api/v1/professionalBookings/${barbeariaId}/${professionalId}/${selectedDate}`, {
         headers: {
           'Authorization': `Bearer ${token}`
         }
-    }).then(res =>{
-      if(res.data.Success === 'Success'){
-        setBarbearias(res.data.Barbearias)
+      })
+      .then(res => {
+        if(res.data.Message === "true"){
+          setBookings(res.data.bookings);
+          // Chamando a função para ordenar os bookings por menor horário
+          orderBookings(bookings);
+        }else{
+          setBookings([])
+          setMessagemNotFound("Nenhum agendamento encontrado")
+        }
+      })
+      .catch(err => console.log(err));
+  }
+
+//Function to expanded booking cards
+  const toggleItem = (itemId) => {
+      if (expandedCardBooking.includes(itemId)) {
+        setExpandedCardBooking(expandedCardBooking.filter(id => id !== itemId));
+      } else {
+        setExpandedCardBooking([...expandedCardBooking, itemId]);
       }
-    }).catch(err =>{
-      console.log("Error", err)
-    })
-}
+  };
+//=========== GET BARB TO PROFESSIONAL ==============
+  const [barbearias, setBarbearias] = useState([]);
+  const [barbeariaSelected, setBarbeariaSelected] = useState();
 
-useEffect(() =>{
-  getBarbearias()
-}, [])
+  const getBarbearias = () =>{
+    axios.get(`${urlApi}/api/v1/listBarbeariaToProfessional/${professionalId}`, {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+      }).then(res =>{
+        if(res.data.Success === 'Success'){
+          setBarbearias(res.data.Barbearias)
+        }
+      }).catch(err =>{
+        console.log("Error", err)
+      })
+  }
 
-const handleBarbeariaSelected = (barbeariaId) =>{
-    setBarbeariaSelected(barbeariaId);
-}
+  useEffect(() =>{
+    getBarbearias()
+  }, [])
+
+  const handleBarbeariaSelected = (barbeariaId) =>{
+      setBarbeariaSelected(barbeariaId);
+  }
 //Function to show small div for unlink barbearia
-const [showBoxUnlinkBarbearia, setshowBoxUnlinkBarbearia] = useState(false);
+  const [showBoxUnlinkBarbearia, setShowBoxUnlinkBarbearia] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [barbeariaId, setBarbeariaId] = useState();
 
-const handleBoxUnlinkClick = () =>{
-  setshowBoxUnlinkBarbearia(!showBoxUnlinkBarbearia)
-}
 
-console.log(showBoxUnlinkBarbearia)
+  const handleBoxUnlinkClick = () =>{
+    setShowBoxUnlinkBarbearia(!showBoxUnlinkBarbearia)
+  }
+  const handleConfirmPasswordClick = (barbeariaId) =>{
+    setBarbeariaId(barbeariaId)
+    setShowConfirmPassword(!showConfirmPassword)
+  }
+
+//============= Section Unlink professional =============
+const [messageUnlinkBarbearia, setMessageUnlinkBarbearia] = useState('');
+
+  const unlinkBarbearia = () =>{
+    axios.delete(`${urlApi}/api/v1/unlinkBarbearia/${barbeariaId}/${professionalId}/${confirmPassword}`, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+      .then(res =>{
+        if(res.data.Success === "Success"){
+          setMessageUnlinkBarbearia('Barbearia desvinculada com sucesso.')
+          setTimeout(() => {
+            setMessageUnlinkBarbearia('');
+            setConfirmPassword('')
+          }, 2000);
+        }
+      }).catch(err =>{
+        setMessageUnlinkBarbearia('Erro ao desvincular a barbearia. Tente novamente mais tarde.')
+        console.error("Error:", err)
+        setTimeout(() => {
+          setMessageUnlinkBarbearia('');
+          setConfirmPassword('')
+        }, 2000);
+      })
+  }
+
 return (
 <>
     <div className="container__main">
@@ -268,6 +306,48 @@ return (
                 <IoIosNotifications/>
             </div>
         </div>
+        {messageUnlinkBarbearia === 'Barbearia desvinculada com sucesso.' ?(
+          <div className="mensagem-sucesso">
+            <MdOutlineDone className="icon__success"/>
+            <p className="text__message">{messageUnlinkBarbearia}</p>
+          </div>
+          ) : (
+          <div className={` ${messageUnlinkBarbearia ? 'mensagem-erro' : ''}`}>
+            <VscError className={`hide_icon__error ${messageUnlinkBarbearia ? 'icon__error' : ''}`}/>
+            <p className="text__message">{messageUnlinkBarbearia}</p>
+          </div>
+        )}
+        {showConfirmPassword &&(
+          <div >
+            <div className="form__change__data">
+            <div className='container__text__change__data'>
+                Digite sua senha para confirmar a alteração
+            </div>
+ 
+           <div className='container__form__change__data'>
+            <input
+                type="password"
+                id="senha"
+                name="senha"
+                value={confirmPassword}
+                className={`input__change__data ${confirmPassword ? 'input__valided':''}`}
+                onChange={(e) => {
+                    const inputValue = e.target.value;
+                    // Limitar a 10 caracteres
+                    const truncatedPasswordConfirm = inputValue.slice(0, 10);
+                    setConfirmPassword(truncatedPasswordConfirm);
+                }}
+                placeholder="Senha atual"
+                maxLength={8}
+                required
+                /><PiPassword className='icon__input__change__data'/>
+                <button className={`Btn__confirm__changes ${confirmPassword ? 'Btn__valided':''}`} onClick={unlinkBarbearia}>
+                    Confirmar
+                </button>
+           </div>
+        </div>
+          </div>
+        )}
         
         {barbearias.length === 1 ?(
           <div className='tittle_menu'>
@@ -284,6 +364,7 @@ return (
             )}
           </>
         )}
+
         <div className="section__barbearia__InHome">
           <div className="section__professional">
 
@@ -294,8 +375,8 @@ return (
               return (
                 <div key={barbearias.barbeariaId} onClick={() => handleBarbeariaSelected(barbearias.barbeariaId)} className={`Box__barbearia__inHome ${barbeariaSelected === barbearias.barbeariaId? 'barbeariaSelected':''}`}>
                     {barbeariaSelected === barbearias.barbeariaId &&(
-                      <div className={`box__unlink__barbearia ${!showBoxUnlinkBarbearia ? 'ocultDivUnlink':''}`}>
-                        <IoIosRemoveCircleOutline/>
+                      <div className={`box__unlink__barbearia ${!showBoxUnlinkBarbearia ? 'ocultDivUnlink':''}`} onClick={() => handleConfirmPasswordClick(barbearias.barbeariaId)}>
+                        <IoIosRemoveCircleOutline />
                         <p>Desvincular</p>
                       </div>
                     )}
