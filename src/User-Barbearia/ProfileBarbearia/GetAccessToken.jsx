ac294073bc4f44fee32f0c0b9313c6560da71a75
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-07-23 20:46:39 -0300

    Update GetAccessToken.jsx

--- src/User-Barbearia/ProfileBarbearia/GetAccessToken.jsx
+++ src/User-Barbearia/ProfileBarbearia/GetAccessToken.jsx
@@ -16,43 +16,6 @@ const GetAccessToken = () => {
   const location = useLocation();
 // APP_USR-7433076748534689-072318-9c0a4826bc237a6b473c73c807bab992-752130654
 // APP_USR-7433076748534689-072318-68846d7876df2114d5441ec7e698f393-752130654
-  useEffect(() => {
-    const getAccessToken = async (authorizationCode) => {
-      const clientId = '7433076748534689';
-      const clientSecret = 'j7cDue7Urw2oKC2WvkLhpOEVL6K8JwHu';
-      const redirectUri = 'https://barbeasy.netlify.app/GetAccessToken';
-      const codeVerifier = localStorage.getItem('code_verifier'); // Recupere o code_verifier salvo
-
-      try {
-        const params = new URLSearchParams();
-        params.append('grant_type', 'authorization_code');
-        params.append('client_id', clientId);
-        params.append('client_secret', clientSecret);
-        params.append('code', authorizationCode);
-        params.append('redirect_uri', redirectUri);
-        params.append('code_verifier', codeVerifier);
-
-        const response = await axios.post('https://api.mercadopago.com/oauth/token', params, {
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-          },
-        });
-
-        console.log(response.data);
-        setAccessToken(response.data.access_token);
-        // Salve o access token conforme necessário
-      } catch (error) {
-        console.error('Error fetching access token:', error);
-      }
-    };
-
-    const params = new URLSearchParams(location.search);
-    const authorizationCode = params.get('code');
-
-    if (authorizationCode) {
-      getAccessToken(authorizationCode);
-    }
-  }, [location.search]);
 
   //Function to save the access token
   const saveAccessToken = () =>{
@@ -76,10 +39,45 @@ const GetAccessToken = () => {
       console.log('nenhum accessToken obtido')
     }
   }
+  
+  useEffect(() => {
+    const getAccessToken = async (authorizationCode) => {
+      const clientId = '7433076748534689';
+      const clientSecret = 'j7cDue7Urw2oKC2WvkLhpOEVL6K8JwHu';
+      const redirectUri = 'https://barbeasy.netlify.app/GetAccessToken';
+      const codeVerifier = localStorage.getItem('code_verifier'); // Recupere o code_verifier salvo
 
-  useEffect(() =>{
-    saveAccessToken()
-  }, [])
+      try {
+        const params = new URLSearchParams();
+        params.append('grant_type', 'authorization_code');
+        params.append('client_id', clientId);
+        params.append('client_secret', clientSecret);
+        params.append('code', authorizationCode);
+        params.append('redirect_uri', redirectUri);
+        params.append('code_verifier', codeVerifier);
+
+        const response = await axios.post('https://api.mercadopago.com/oauth/token', params, {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        });
+
+        setAccessToken(response.data.access_token);
+        saveAccessToken()
+
+      } catch (error) {
+        console.error('Error fetching access token:', error);
+      }
+    };
+
+    const params = new URLSearchParams(location.search);
+    const authorizationCode = params.get('code');
+
+    if (authorizationCode) {
+      getAccessToken(authorizationCode);
+    }
+  }, [location.search]);
+
 
   return (
     <div>
