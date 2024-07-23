5486a39b7c5c6b46d1f7448bd24ac2f262b38bc0
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-07-23 19:44:29 -0300

    update request

--- src/User-Barbearia/ProfileBarbearia/GetAccessToken.jsx
+++ src/User-Barbearia/ProfileBarbearia/GetAccessToken.jsx
@@ -1,14 +1,10 @@
-import { useState, useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom"
-
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import axios from "axios";
 
-export default function GetAccessToken (){
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  // obter access_token
+const GetAccessToken = () => {
   const [accessToken, setAccessToken] = useState(null);
+  const location = useLocation();
 
   useEffect(() => {
     const getAccessToken = async (authorizationCode) => {
@@ -16,22 +12,19 @@ export default function GetAccessToken (){
       const clientSecret = 'j7cDue7Urw2oKC2WvkLhpOEVL6K8JwHu';
       const redirectUri = 'https://barbeasy.netlify.app/GetAccessToken';
       const codeVerifier = localStorage.getItem('code_verifier'); // Recupere o code_verifier salvo
-
       try {
         const params = new URLSearchParams();
         params.append('grant_type', 'authorization_code');
         params.append('client_id', clientId);
         params.append('client_secret', clientSecret);
-        params.append('code', authorizationCode);         
+        params.append('code', authorizationCode);
         params.append('redirect_uri', redirectUri);
         params.append('code_verifier', codeVerifier);
-
         const response = await axios.post('https://api.mercadopago.com/oauth/token', params, {
           headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
           },
         });
-
         console.log(response.data);
         setAccessToken(response.data.access_token);
         // Salve o access token conforme necessário
@@ -39,27 +32,27 @@ export default function GetAccessToken (){
         console.error('Error fetching access token:', error);
       }
     };
-
     const params = new URLSearchParams(location.search);
     const authorizationCode = params.get('code');
-console.log(authorizationCode)
     if (authorizationCode) {
       getAccessToken(authorizationCode);
     }
   }, [location.search]);
 
-  return(
+  return (
     <div>
       olá mundo!
       {accessToken ? (
         <div>
-          {accessToken}
+          Access Token: {accessToken}
         </div>
-      ):(
+      ) : (
         <div>
-          obtendo access token
+          Obtendo access token...
         </div>
       )}
     </div>
-  )
-}
+  );
+};
+
+export default GetAccessToken;
