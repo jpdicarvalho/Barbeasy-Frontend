e49c4b4983ea8166491db28a0ec6bd02971aec74
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-07-23 20:02:55 -0300

    Creating request to save access token

--- src/User-Barbearia/ProfileBarbearia/GetAccessToken.jsx
+++ src/User-Barbearia/ProfileBarbearia/GetAccessToken.jsx
@@ -3,9 +3,19 @@ import { useLocation } from "react-router-dom";
 import axios from "axios";
 
 const GetAccessToken = () => {
+
+  const urlApi = 'https://barbeasy.up.railway.app'
+
+  //Buscando informações do usuário logado
+  const token = localStorage.getItem('token');
+  const userData = localStorage.getItem('dataBarbearia');//Obtendo os dados salvo no localStorage
+  const userInformation = JSON.parse(userData);//trasnformando os dados para JSON
+  const barbeariaId = userInformation.barbearia[0].id;
+
   const [accessToken, setAccessToken] = useState(null);
   const location = useLocation();
-
+// APP_USR-7433076748534689-072318-9c0a4826bc237a6b473c73c807bab992-752130654
+// APP_USR-7433076748534689-072318-68846d7876df2114d5441ec7e698f393-752130654
   useEffect(() => {
     const getAccessToken = async (authorizationCode) => {
       const clientId = '7433076748534689';
@@ -38,6 +48,30 @@ const GetAccessToken = () => {
       getAccessToken(authorizationCode);
     }
   }, [location.search]);
+
+  useEffect(() =>{
+    const saveAccessToken = () =>{
+      if(accessToken){
+          const values = {
+            barbeariaId,
+            accessToken
+          }
+          axios.put(`${urlApi}/api/v1/saveAccessToken`, values, {
+            headers: {
+              'Authorization': `Bearer ${token}`
+            }
+          }).then(res =>{
+            if(res.data.Sucess === 'Sucess'){
+              console.log('AcessToken salvo com sucesso')
+            }
+          }).catch(err =>{
+            console.log('ERRO:',err)
+          })
+      }
+    }
+
+    saveAccessToken()
+  }, [])
 
   return (
     <div>
