9db4d4030aef978a248c80e668964dadd0a0a5ba
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-07-29 08:47:21 -0300

    message ui/ux to time over

--- src/User-Client-Barbearia/PaymentsScreen/PaymentScreen.jsx
+++ src/User-Client-Barbearia/PaymentsScreen/PaymentScreen.jsx
@@ -8,6 +8,7 @@ import { PiCopySimple } from "react-icons/pi";
 import { IoTicketOutline } from "react-icons/io5";
 import { RiSecurePaymentLine } from "react-icons/ri";
 import { IoIosCheckmarkCircleOutline } from "react-icons/io";
+import { RxCounterClockwiseClock } from "react-icons/rx";
 
 import './PaymentScreen.css'
 
@@ -30,6 +31,8 @@ export default function PaymentScreen(){
         navigate("/Home ");
     };
 
+    const [deletedPreBooking, setDeletedPreBooking] = useState(false)
+
     const deletePreBooking = () =>{
         axios.delete(`${urlApi}/api/v1/delePreBooking/${paymentObject.id}/${identificationToken}`,{
             headers: {
@@ -37,10 +40,11 @@ export default function PaymentScreen(){
               }
         }).then(res =>{
             if(res.data.Success === true){
-                console.log('deu certo')
+                setDeletedPreBooking(true)
             }
         }).catch(err =>{
             console.log('deu ruim')
+            setDeletedPreBooking(false)
         })
     }
     //Function to calculete the time difference betwen date_of_expiration and current date
@@ -61,7 +65,7 @@ export default function PaymentScreen(){
           setSeconds((prevSeconds) => {
             if (prevSeconds <= 0) {
                 clearInterval(timer);
-                //handleBackClick()
+                deletePreBooking()
                 return 0;
             }
             return prevSeconds - 1;
@@ -146,100 +150,110 @@ export default function PaymentScreen(){
 
     return(
         <div className="container__master">
-            {paymentUpdated ? (
-                <div className="container__payment__approved">
-                    <IoIosCheckmarkCircleOutline className="icon__CheckmarkCircleOutline"/>
-                    <p className="text__one__conection__succesfuly">Pagamento recebido!</p>
-                    <p className="text__two__conection__succesfuly">Seu agendamento foi confirmado! Verifique a data e o horário para não perder sua reserva.</p>
-                    <div className="Box__btn__back__Booking__Details" onClick={handleBackClick}>
-                        <button className="Btn__back__Booking__Details" >
-                            Voltar
-                        </button>
-                    </div>
+            {!deletedPreBooking ? (
+                <div className="teste">
+                    {paymentUpdated ? (
+                        <div className="container__payment__approved">
+                            <IoIosCheckmarkCircleOutline className="icon__CheckmarkCircleOutline"/>
+                            <p className="text__one__conection__succesfuly">Pagamento recebido!</p>
+                            <p className="text__two__conection__succesfuly">Seu agendamento foi confirmado! Verifique a data e o horário para não perder sua reserva.</p>
+                            <div className="Box__btn__back__Booking__Details" onClick={handleBackClick}>
+                                <button className="Btn__back__Booking__Details" >
+                                    Voltar
+                                </button>
+                            </div>
+                        </div>
+                    ):(
+                        <div className="container__payment__screen">
+                            <div className="header__payment__screen">
+                                <h2>Babreasy</h2>
+                                <div className="box__pay__secure">
+                                    <RiSecurePaymentLine className="icon__pay__secure"/>
+                                    <p>Pague com segurança</p>
+                                </div>
+                            </div>
+                            <div className="section__value__payment">
+                                <h3 className="value__payment">Pague {serviceValues.servicePrice} via Pix</h3>
+                                <p className="date_of_expiration">{seconds > 0 ? `Vencimento em ${seconds} segundos` : 'Tempo esgotado :('}</p>
+                            </div>
+                            <div className="Box__qr_code_base64">
+                                <img className="inner__qr_code_base64" src={`data:image/png;base64,${qr_code_base64}`} alt="QR Code Base64" />
+                            </div>
+                            <div className="Box__qr_code__to__copy">
+                                <h5>Código de pagamento</h5>
+                                {copyMessage &&(
+                                    <div className="box__text__copied">
+                                        <p className="text__copied">Código de pagamento copiado!</p>
+                                    </div>
+                                )}
+                                <div className="box__value__qr_code">
+                                    <p className="value__qr_code">{qr_code}</p>
+                                    <PiCopySimple className="icon__PiCopySimple" onClick={() => handleCopy(qr_code)}/>
+                                </div>
+                            </div>
+                            <div className="section__how__pay">
+                                <h3>Como Pagar?</h3>
+                                <div className="box__instructions__how__pay">
+                                    <div className="inner__instruction__how__pay">
+                                        <p className="number__one__instruction">1</p>
+                                        <p className="text__instruction">Entre no app ou site do seu banco e escolha a opção de pagamento via Pix.</p>
+                                    </div>
+                                    <div className="inner__instruction__how__pay">
+                                        <p className="number__two__instruction">2</p>
+                                        <p className="text__instruction">Escaneie o código QR ou copie e cole o código de pagamento.</p>
+                                    </div>
+                                    <div className="inner__instruction__how__pay">
+                                        <p className="number__three__instruction">3</p>
+                                        <p className="text__instruction">Pronto! O pagamento será creditado na hora e você receberá um e-mail de confirmação.</p>
+                                    </div>
+                                    
+                                    <p className="text__information__pix">O Pix tem um limite diário de transferências. Para mais informações, por favor, consulte seu banco.</p>
+                                </div>
+                            </div>
+                            <div className="section__details__service">
+                                <IoTicketOutline className="icon__IoTicketOutline"/>
+                                <h2>Detalhes do seu serviço</h2>
+                            </div>
+                            <div className="values__details__service">
+                                <p>Barbearia</p>
+                                <p>{serviceValues.barbeariaName}</p>
+                            </div>
+                            <div className="values__details__service">
+                                <p>Profissional</p>
+                                <p>{serviceValues.professionalName}</p>
+                            </div>
+                            <div className="values__details__service">
+                                <p>{serviceValues.serviceName}</p>
+                                <p>{serviceValues.servicePrice}</p>
+                            </div>
+                            <div className="values__details__service">
+                                <p>Duração</p>
+                                <p>{serviceValues.serviceDuration}min</p>
+                            </div>
+                            <div className="values__details__service">
+                                <p>Data</p>
+                                <p>{serviceValues.selectedDay}</p>
+                            </div>
+                            <div className="values__details__service">
+                                <p>Início</p>
+                                <p>{serviceValues.timeSelected} h</p>
+                            </div>
+                            <div className="Box__btn__back__Booking__Details" style={{marginBottom: '20px'}} onClick={handleBackClick}>
+                                <button className="Btn__back__Booking__Details" >
+                                    Voltar
+                                </button>
+                            </div>
+                        </div>
+                    )}
                 </div>
             ):(
-                <div className="container__payment__screen">
-                    <div className="header__payment__screen">
-                        <h2>Babreasy</h2>
-                        <div className="box__pay__secure">
-                            <RiSecurePaymentLine className="icon__pay__secure"/>
-                            <p>Pague com segurança</p>
-                        </div>
-                    </div>
-                    <div className="section__value__payment">
-                        <h3 className="value__payment">Pague {serviceValues.servicePrice} via Pix</h3>
-                        <p className="date_of_expiration">{seconds > 0 ? `Vencimento em ${seconds} segundos` : 'Tempo esgotado :('}</p>
-                    </div>
-                    <div className="Box__qr_code_base64">
-                        <img className="inner__qr_code_base64" src={`data:image/png;base64,${qr_code_base64}`} alt="QR Code Base64" />
-                    </div>
-                    <div className="Box__qr_code__to__copy">
-                        <h5>Código de pagamento</h5>
-                        {copyMessage &&(
-                            <div className="box__text__copied">
-                                <p className="text__copied">Código de pagamento copiado!</p>
-                            </div>
-                        )}
-                        <div className="box__value__qr_code">
-                            <p className="value__qr_code">{qr_code}</p>
-                            <PiCopySimple className="icon__PiCopySimple" onClick={() => handleCopy(qr_code)}/>
-                        </div>
-                    </div>
-                    <div className="section__how__pay">
-                        <h3>Como Pagar?</h3>
-                        <div className="box__instructions__how__pay">
-                            <div className="inner__instruction__how__pay">
-                                <p className="number__one__instruction">1</p>
-                                <p className="text__instruction">Entre no app ou site do seu banco e escolha a opção de pagamento via Pix.</p>
-                            </div>
-                            <div className="inner__instruction__how__pay">
-                                <p className="number__two__instruction">2</p>
-                                <p className="text__instruction">Escaneie o código QR ou copie e cole o código de pagamento.</p>
-                            </div>
-                            <div className="inner__instruction__how__pay">
-                                <p className="number__three__instruction">3</p>
-                                <p className="text__instruction">Pronto! O pagamento será creditado na hora e você receberá um e-mail de confirmação.</p>
-                            </div>
-                            
-                            <p className="text__information__pix">O Pix tem um limite diário de transferências. Para mais informações, por favor, consulte seu banco.</p>
-                        </div>
-                    </div>
-                    <div className="section__details__service">
-                        <IoTicketOutline className="icon__IoTicketOutline"/>
-                        <h2>Detalhes do seu serviço</h2>
-                    </div>
-                    <div className="values__details__service">
-                        <p>Barbearia</p>
-                        <p>{serviceValues.barbeariaName}</p>
-                    </div>
-                    <div className="values__details__service">
-                        <p>Profissional</p>
-                        <p>{serviceValues.professionalName}</p>
-                    </div>
-                    <div className="values__details__service">
-                        <p>{serviceValues.serviceName}</p>
-                        <p>{serviceValues.servicePrice}</p>
-                    </div>
-                    <div className="values__details__service">
-                        <p>Duração</p>
-                        <p>{serviceValues.serviceDuration}min</p>
-                    </div>
-                    <div className="values__details__service">
-                        <p>Data</p>
-                        <p>{serviceValues.selectedDay}</p>
-                    </div>
-                    <div className="values__details__service">
-                        <p>Início</p>
-                        <p>{serviceValues.timeSelected} h</p>
-                    </div>
-                    <div className="Box__btn__back__Booking__Details" style={{marginBottom: '20px'}} onClick={handleBackClick}>
-                        <button className="Btn__back__Booking__Details" >
-                            Voltar
-                        </button>
-                    </div>
+                <div className="section__time__over">
+                    <RxCounterClockwiseClock className="icon__RxCounterClockwiseClock"/>
+                    <p className="text__one__conection__succesfuly">Tempo esgotado...</p>
+                    <p className="text__two__conection__succesfuly">O tempo para a realização do pagamento expirou. Mas não tem problema! Você pode tentar novamente</p>
                 </div>
             )}
-            
+        
         </div>
         
     )
